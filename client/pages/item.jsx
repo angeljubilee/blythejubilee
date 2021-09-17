1afc59b21c19afbdf44940a0671f1b06d920cfaa
Angel Lee <angeljubilee@gmail.com>
2021-09-17 00:31:02 +0000

    Fixed bug with selecting size on item page.  Updated styling to look good in mobile and desktop

--- client/pages/item.jsx
+++ client/pages/item.jsx
@@ -14,18 +14,25 @@ export default class Item extends React.Component {
     this.addVariation = this.addVariation.bind(this);
     this.addQuantity = this.addQuantity.bind(this);
 
-    this.select = React.createRef();
+    this.select1 = React.createRef();
   }
 
   componentDidMount() {
     fetch(`/api/items/${this.props.itemId}`)
       .then(res => res.json())
-      .then(item => this.setState({ item }));
+      .then(item => {
+        const defaultVars = { };
+        for (const key in item.vars) {
+          defaultVars[key] = item.vars[key][0];
+        }
+        this.setState({ vars: defaultVars });
+        this.setState({ item });
+      });
   }
 
   componentDidUpdate() {
-    if (this.select.current) {
-      M.FormSelect.init(this.select.current);
+    if (this.select1.current) {
+      M.FormSelect.init(this.select1.current);
     }
   }
 
@@ -34,16 +41,17 @@ export default class Item extends React.Component {
   }
 
   addVariation(event) {
+    const variation = this.state.item.vars[event.target.name].find(v => v.varId === parseInt(event.target.value));
     const newVars = { ...this.state.vars };
-    newVars[event.target.name] = event.target.value;
+    newVars[event.target.name] = variation;
     this.setState({ vars: newVars });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    const { itemId, url, title, price } = this.state.item;
+    const { itemId, url, title, price, numInStock } = this.state.item;
     const { qty, vars } = this.state;
-    this.props.addToCart({ itemId, url, title, price, qty, vars });
+    this.props.addToCart({ itemId, url, title, price, numInStock, qty, vars });
 
   }
 
@@ -51,72 +59,75 @@ export default class Item extends React.Component {
     if (!this.state.item) {
       return <div>Product not found</div>;
     }
+    const opts = {};
+    for (const key in this.state.item.vars) {
+      const opt = this.state.item.vars[key].map((variation, index) => {
+        const { value, varId } = variation;
+        return (
+          <div className="col s4 m2" key={index}>
+            <label>
+              <input name={key}
+                type="radio"
+                value={varId}
+                checked={value === this.state.vars[key].value}
+                onChange={this.addVariation} />
+              <span>{value}</span>
+            </label>
+          </div>
+        );
+      });
+      opts[key] = opt;
+    }
+
+    const variations = [];
+    for (const key in this.state.item.vars) {
+      variations.push(
+        <div className="row" key={key}>
+          <div className="col s12">
+            <p className="label">{key}</p>
+            <div className="row">
+              {opts[key]}
+            </div>
+          </div>
+        </div>
+      );
+    }
 
     return (
-      <>
         <div className="container-max-70">
-          <img className="img-1-4" src={this.state.item.url}></img>
-        </div>
-        <form onSubmit={this.handleSubmit}>
-          <div className="container">
+          <div className="product-details">
             <div className="row">
-              <div className="col s12">
-                <h6>{this.state.item.title.slice(0, 38)}...</h6>
-                <h6>{this.state.item.price}</h6>
+              <div className="col s12 m6">
+                <img className="img-1-2" src={this.state.item.url}></img>
+              </div>
+              <div className="col s12 m6">
+                <form className="no-autoinit margin-left-1" onSubmit={this.handleSubmit}>
+                  <h6>{this.state.item.title}</h6>
+                  <h6>{this.state.item.price}</h6>
+                  {variations}
+                  <div className="row">
+                    <div className="col s4 m2">
+                      <label>Qty</label>
+                      <select ref={this.select1} onChange={this.addQuantity} className="qty-input">
+                        <QtyOptions num={this.state.item.numInStock} />
+                      </select>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col s11 hide-on-med-and-up">
+                      <button>Add to Cart</button>
+                    </div>
+                    <div className="col s4 right hide-on-small-only">
+                      <button>Add to Cart</button>
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
-            <Variations vars={this.state.item.vars} addVariation={this.addVariation}/>
-            <div className="row">
-              <div className="input-field col s4 m2">
-                <select ref={this.select} onChange={this.addQuantity}>
-                  <QtyOptions num={this.state.item.numInStock} />
-                </select>
-              <label>Qty</label>
-              </div>
-            </div>
-          </div>
-          <div className="container-max-70">
-            <button type="submit" className="float-right">Add to Cart</button>
-          </div>
-        </form>
-      </>
-    );
-  }
-}
-
-function Variations(props) {
-  const variations = [];
-  let key;
-  for (key in props.vars) {
-    variations.push(
-      <div className="row" key={key}>
-        <div className="col s12">
-          <p className="label">{key}</p>
-          <div className="row">
-            <VarOptions name={key} options={props.vars[key]} addVariation={props.addVariation} />
           </div>
         </div>
-      </div>
     );
   }
-  return variations;
-
-}
-
-function VarOptions(props) {
-  const options = props.options.map((variation, index) => {
-    return (
-      <div className="col s4 m1" key={index}>
-        <label>
-          <input name={props.name} type="radio" value={variation.varId}
-            onChange={props.addVariation}/>
-          <span>{variation.value}</span>
-        </label>
-      </div>
-
-    );
-  });
-  return options;
 }
 
 function QtyOptions(props) {
