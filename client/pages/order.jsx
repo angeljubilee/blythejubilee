3af0056eaea8400cab9f6fb4202b6df571986f79
Angel Lee <angeljubilee@gmail.com>
2021-10-05 13:28:47 -0700

    Updated order screen to improve appearance on mobile screens

--- client/pages/order.jsx
+++ client/pages/order.jsx
@@ -4,13 +4,13 @@ export default function Order(props) {
   return (
     <div className="container margin-top-1">
       <div className="row">
-        <div className="col s3"></div>
-        <div className="col s6">
-          <h3 className="bold-text margin-bottom-1">Thank you for your order!</h3>
-          <h4 className="bold-text margin-bottom-1">Order #: {props.orderId}</h4>
-          <h5>We will contact you as soon as your package has shipped.</h5>
+        <div className="col m3"></div>
+        <div className="col s12 m6">
+          <h4 className="bold-text margin-bottom-1">Thank you for your order!</h4>
+          <h5 className="bold-text margin-bottom-1">Order #: {props.orderId}</h5>
+          <h6>We will contact you as soon as your package has shipped.</h6>
         </div>
-        <div className="col s3"></div>
+        <div className="col m3"></div>
       </div>
     </div>
   );
